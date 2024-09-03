0450ef96106105eab4f7d01f2be00f07b01f110a
matin-reza <31912502+matin-reza@users.noreply.github.com>
2024-09-03 12:48:27 +0330

    Update custom_phonemizer.py
    ...

--- TTS/tts/utils/text/phonemizers/custom_phonemizer.py
+++ TTS/tts/utils/text/phonemizers/custom_phonemizer.py
@@ -1,16 +1,25 @@
 # File: custom_phonemizer.py
 
 from PersianG2p import Persian_g2p_converter
+from TTS.tts.utils.text.phonemizers.base import BasePhonemizer
 
-class CustomPhonemizer:
-    def __init__(self, use_large=False):
-        # Initialize an instance of Persian_g2p_converter
-        self.converter = Persian_g2p_converter(use_large=use_large)
+_DEF_FA_PUNCS = "؟ ! ) ("
+class CustomPhonemizer(BasePhonemizer):
+     language = "fa"
+    def __init__(self, punctuations=_DEF_FA_PUNCS, keep_puncs=False, **kwargs):  # pylint: disable=unused-argument
+        super().__init__(self.language, punctuations=punctuations, keep_puncs=keep_puncs)
+        self.converter = Persian_g2p_converter(True)
     
     def phonemize(self, text):
         # Call the appropriate method from Persian_g2p_converter to perform phonemization
         # Use 'transliterate' if that's the method you want; adjust if necessary
         return self.converter.transliterate(text, tidy=False)  # Adjust method if necessary
+
+    def version(self) -> str:
+        return "0.0.2"
+
+    def is_available(self) -> bool:
+        return True
 
 # Example usage
 if __name__ == "__main__":
